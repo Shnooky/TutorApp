73b9ed6efcebff88b0dc93432fd9227a184b7c13
Jacob Meyerowitz <jacob.meyerowitz@gmail.com>
2017-10-15 18:56:59 +1100

    Started to create a home screen, buttons aren't working yet.

--- app/src/main/java/com/example/xwc/tutorapp/Controllers/MainActivity.java
+++ app/src/main/java/com/example/xwc/tutorapp/Controllers/MainActivity.java
@@ -1,17 +1,54 @@
 package com.example.xwc.tutorapp.Controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.xwc.tutorapp.R;
 
-public class MainActivity extends AppCompatActivity {
-//Testing the git
-    //Jacob added this comment
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mClassList;
+
+    private Button mStudentProfile;
+
+    private Button mNameGame;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // RECHARGE
+
+        mClassList = (Button) findViewById(R.id.classes);
+        mClassList.setOnClickListener(this);
+        mStudentProfile = (Button) findViewById(R.id.student);
+        mStudentProfile.setOnClickListener(this);
+        mNameGame = (Button) findViewById(R.id.nameGame);
+        mNameGame.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+
+        switch(v.getId()) {
+            case R.id.classes:
+                intent = new Intent(this, ClassList.class);
+                break;
+            case R.id.student:
+                intent = new Intent(this, StudentProfile.class);
+                break;
+            case R.id.nameGame:
+                break;
+            default:
+
+        }
+        if (intent !=null) {
+            startActivity(intent);
+        }
+
     }
 }
