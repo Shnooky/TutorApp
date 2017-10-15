cf8a8ad80e13570b7fb64a7fae75c6ca166bf98e
Jacob Meyerowitz <jacob.meyerowitz@gmail.com>
2017-10-15 14:56:13 +1100

    Testing the git

--- app/src/main/java/com/example/xwc/tutorapp/MainActivity.java
+++ app/src/main/java/com/example/xwc/tutorapp/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//Testing the git
+    //Jacob added this comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
