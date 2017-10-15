6f716bc7f56d1e5d2e59f9ec9523aae5d9b988be
Jacob Meyerowitz <jacob.meyerowitz@gmail.com>
2017-10-15 19:19:41 +1100

    Fixed ClassList crash issue

---
+++ app/src/main/java/com/example/xwc/tutorapp/Adapters/ClassAdapter.java
@@ -0,0 +1,48 @@
+package com.example.xwc.tutorapp.Adapters;
+
+/**
+ * Created by Jacob on 15/10/2017.
+ */
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.xwc.tutorapp.Model.Class;
+import com.example.xwc.tutorapp.R;
+
+import java.util.List;
+
+public class ClassAdapter extends ArrayAdapter<Class> {
+
+    public ClassAdapter(Context context, List<Class> classes) {
+        super(context, 0, classes);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.view_classes, parent, false);
+        }
+
+        Class currentClass = getItem(position);
+
+        TextView classView = (TextView) listItemView.findViewById(R.id.className);
+        classView.setText(currentClass.getClassId() + " " + currentClass.getDay() + " " + currentClass.getStartTime() +
+                "-" + currentClass.getEndTime());
+
+
+        TextView gradeView = (TextView) listItemView.findViewById(R.id.avGrade);
+        gradeView.setText(Double.toString(currentClass.getAverageGrade()));
+
+        TextView numStuView = (TextView) listItemView.findViewById(R.id.numberStudents);
+        gradeView.setText(Integer.toString(currentClass.getNumStu()));
+
+        return listItemView;
+    }
+
+
+}
--- app/src/main/java/com/example/xwc/tutorapp/Controllers/ClassList.java
+++ app/src/main/java/com/example/xwc/tutorapp/Controllers/ClassList.java
@@ -1,50 +1,47 @@
 package com.example.xwc.tutorapp.Controllers;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
+import android.support.v4.app.BundleCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.xwc.tutorapp.Adapters.ClassAdapter;
 import com.example.xwc.tutorapp.Model.Class;
 import com.example.xwc.tutorapp.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Jacob on 15/10/2017.
  */
 
-public class ClassList extends ArrayAdapter<Class> {
+public class ClassList extends AppCompatActivity {
 
-    public ClassList(Context context, List<Class> classes) {
-        super(context,0,classes);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.class_activity);
+
+        ArrayList<Class> classes = new ArrayList<Class>();
+
+        ListView classesListView = (ListView) findViewById(R.id.list);
+
+        ClassAdapter adapter = new ClassAdapter(this, classes);
+
+        classesListView.setAdapter(adapter);
+
+
     }
 
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View listItemView = convertView;
-        if(listItemView==null) {
-            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.view_classes, parent, false);
-        }
-
-        Class currentClass = getItem(position);
-
-        TextView classView = (TextView) listItemView.findViewById(R.id.className);
-        classView.setText(currentClass.getClassId()+" "+currentClass.getDay()+" "+currentClass.getStartTime()+
-        "-"+currentClass.getEndTime());
-
-
-        TextView gradeView = (TextView) listItemView.findViewById(R.id.avGrade);
-        gradeView.setText(Double.toString(currentClass.getAverageGrade()));
-
-        TextView numStuView = (TextView) listItemView.findViewById(R.id.numberStudents);
-        gradeView.setText(Integer.toString(currentClass.getNumStu()));
-
-        return listItemView;
-    }
 
 
 
