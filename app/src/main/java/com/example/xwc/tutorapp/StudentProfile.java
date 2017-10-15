c4e8958c54aeadc9e0458685503a47a1ee8b8630
Jacob Meyerowitz <jacob.meyerowitz@gmail.com>
2017-10-15 16:40:24 +1100

    Student profile screen added

--- app/src/main/java/com/example/xwc/tutorapp/StudentProfile.java
+++ app/src/main/java/com/example/xwc/tutorapp/StudentProfile.java
@@ -3,11 +3,11 @@ package com.example.xwc.tutorapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StudentActivity extends AppCompatActivity {
+public class StudentProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student);
+        setContentView(R.layout.student_profile);
     }
 }
