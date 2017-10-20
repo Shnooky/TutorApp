f791aafcff127a3db47d758db377e7a54a459870
Jacob Meyerowitz <jacob.meyerowitz@gmail.com>
2017-10-21 02:04:05 +1100

    More UI Stuff

--- app/src/main/java/com/example/xwc/tutorapp/Adapters/TutorialAdapter.java
+++ app/src/main/java/com/example/xwc/tutorapp/Adapters/TutorialAdapter.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.xwc.tutorapp.Model.Tutorial;
@@ -16,6 +18,7 @@ import com.example.xwc.tutorapp.R;
 import org.w3c.dom.Text;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Jacob on 15/10/2017.
@@ -45,6 +48,23 @@ public class TutorialAdapter extends ArrayAdapter<Tutorial> {
 
         TextView latesView = (TextView) listItemView.findViewById(R.id.numLates);
         latesView.setText(Integer.toString(currentTutorial.getLates()));
+
+        ImageView image = (ImageView) listItemView.findViewById(R.id.tutImage);
+        Random rn = new Random();
+        switch(rn.nextInt(3-1+1)+1) {
+            case 1:
+                image.setColorFilter(ContextCompat.getColor(getContext(),R.color.colorAccent));
+                break;
+            case 2:
+                image.setColorFilter(ContextCompat.getColor(getContext(),R.color.colorPrimary));
+                break;
+            case 3:
+                image.setColorFilter(ContextCompat.getColor(getContext(),R.color.colorPrimaryDark));
+                break;
+            default:
+                break;
+
+        }
         
 
 
